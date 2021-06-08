1caa2ef54ba1eefe6dc538595b9b2b3907905689
angelo.p <mrpalaiologos@gmail.com>
2021-06-08 16:49:04 +0200

    everything except the map is done

--- src/components/Flaglist.js
+++ src/components/Flaglist.js
@@ -1,11 +1,17 @@
 import React from "react";
+var countriesList = require("countries-list");
 
 const Flaglist = ({ lista }) => {
+  //for later to implement emojis from this library
+  const emoj = countriesList.getEmojiFlag("UA");
+  console.log(emoj);
+
   const flags = lista.map((obj, i) => {
     console.log(obj);
-    const { name, flag, capital, population, region } = obj;
+    const { name, flag, capital, population, region, languages } = obj;
     return (
       <div key={i}>
+        {/* languages: {languages.map(el)} */}
         {name}&nbsp; Region:&nbsp; {region} &nbsp; Capital:&nbsp; {capital}
         &nbsp; Population: &nbsp;{population}&nbsp;
         <img src={flag} alt={name} className="flag" />
